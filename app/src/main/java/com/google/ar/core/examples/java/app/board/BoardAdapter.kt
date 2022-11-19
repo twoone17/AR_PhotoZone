e488c58f6ca483e3b726b3d287c0fcd5ec2d1343
Owen-Choi <oldstyle482@gmail.com>
2022-11-19 16:35:45 +0900

    [ADD] 어뎁터, 리사이클러뷰 xml 오류 전 코드

---
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/BoardAdapter.kt
@@ -0,0 +1,37 @@
+package com.google.ar.core.examples.java.app.board
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.google.ar.core.examples.java.geospatial.R
+
+class ProfileAdapter(private val context: Context) : RecyclerView.Adapter<ProfileAdapter.ViewHolder>() {
+
+    var datas = mutableListOf<BoardData>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_recycler_ex,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = datas.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val imgProfile: ImageView = itemView.findViewById(R.id.img_rv_photo)
+
+        fun bind(item: BoardData) {
+            Glide.with(itemView).load(item.img).into(imgProfile)
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/BoardData.kt
@@ -0,0 +1,7 @@
+package com.google.ar.core.examples.java.app.board
+
+data class BoardData(
+    val name : String,
+    val age : Int,
+    val img : Int
+)
