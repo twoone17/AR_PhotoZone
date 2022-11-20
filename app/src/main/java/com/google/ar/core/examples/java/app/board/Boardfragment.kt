d61a5d32986c8b344581bee838c104e57f6f0ff9
Owen-Choi <oldstyle482@gmail.com>
2022-11-20 16:41:20 +0900

    [ADD] floating point button 적용 전

--- app/src/main/java/com/google/ar/core/examples/java/app/board/Boardfragment.kt
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/Boardfragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.ar.core.examples.java.geospatial.R
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_board.*
 
 class Boardfragment : Fragment() {
 
@@ -23,7 +24,14 @@ class Boardfragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val v =  inflater.inflate(R.layout.fragment_board, container, false)
+        // 리사이클러뷰 초기화 -> 파이어베이스 데이터 로드
         initRecycler(v)
+
+        uploadButton.setOnClickListener{
+            val nextIntent = Intent(requireContext(), UploadActivity::class.java)
+            startActivity(nextIntent)
+        }
+
         return v
     }
 
---
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/UploadActivity.kt
@@ -0,0 +1,12 @@
+package com.google.ar.core.examples.java.app.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.ar.core.examples.java.geospatial.R
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
