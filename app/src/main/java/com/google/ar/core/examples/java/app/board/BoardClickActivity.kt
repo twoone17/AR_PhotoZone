7c009ff9a5493163f692ce5d3a4706eaca21491e
Owen-Choi <oldstyle482@gmail.com>
2022-11-20 16:14:41 +0900

    테마 변경

--- app/src/main/java/com/google/ar/core/examples/java/app/board/BoardClickActivity.kt
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/BoardClickActivity.kt
@@ -3,7 +3,9 @@ package com.google.ar.core.examples.java.app.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.google.ar.core.examples.java.geospatial.R
+import kotlinx.android.synthetic.main.activity_board_click.*
 
 class BoardClickActivity : AppCompatActivity() {
 
@@ -14,8 +16,13 @@ class BoardClickActivity : AppCompatActivity() {
 
         val intent = intent
         val boardData = intent.getSerializableExtra("boardData") as BoardData?
+
         if (boardData != null) {
-            Log.e(TAG, "onCreate: " + boardData.description)
+            Glide.with(this).load(boardData.img).error(R.drawable.ic_baseline_error_outline_24).into(post_image)
+            username.text = boardData.userId
+            description.text = boardData.description
+            likes.text = boardData.likes.toString()
         }
+
     }
 }
