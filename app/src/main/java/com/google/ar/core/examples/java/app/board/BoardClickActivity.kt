aabbbeef05886ff7d10bb16a5bca6e04ff04a6e3
Owen-Choi <oldstyle482@gmail.com>
2022-11-22 19:40:39 +0900

    [ADD] 좋아요 개수 서버에서 받아오기

--- app/src/main/java/com/google/ar/core/examples/java/app/board/BoardClickActivity.kt
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/BoardClickActivity.kt
@@ -9,6 +9,8 @@ import com.google.ar.core.examples.java.geospatial.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_board_click.*
@@ -18,6 +20,9 @@ class BoardClickActivity : AppCompatActivity() {
     val TAG = "BoardClickActivity"
     private lateinit var auth: FirebaseAuth
     val db = FirebaseFirestore.getInstance()
+    var liked = true
+    var saved = true
+    var likeCount = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_click)
@@ -44,10 +49,12 @@ class BoardClickActivity : AppCompatActivity() {
             publisher.text = boardData.userId
             var likeString = boardData.likes.toString() + " likes"
             likes.text = likeString
-            likes.text = likeString
     }
 
     private fun isLikePressed(boardData: BoardData) {
+
+        getLikeCountsFromServer(boardData)
+
         auth = Firebase.auth
         // 임시 자동 로그인, 로그인 화면 구현 시 삭제 예정
         auth.signInWithEmailAndPassword("oldstyle4@naver.com", "2580as2580@").addOnSuccessListener {
@@ -56,20 +63,60 @@ class BoardClickActivity : AppCompatActivity() {
                 val uid = currentUser.uid
                 var likesReference : CollectionReference = db.collection("app_board").document(boardData.documentId).collection("Likes")
                 likesReference.document(uid).get().addOnSuccessListener { result ->
-                    Log.e(TAG, "isLikePressed: " + result.reference)
+                    if(result.exists()) {
+                        like.setImageResource(R.drawable.ic_liked)
+                        liked = false
+                    } else {
+                        like.setImageResource(R.drawable.ic_like)
+                        liked = true
+                    }
+                }.addOnFailureListener { error ->
+                    Log.e(TAG, "isLikePressed : " + error )
                 }
             }
         }
         // 현재 유저가 null이 아니어야 모든 로직이 실행될 수 있다.
     }
 
+
+    private fun getLikeCountsFromServer(boardData: BoardData) {
+        var likesReference : CollectionReference = db.collection("app_board").document(boardData.documentId).collection("Likes")
+        var forUpdateLikesCountDocument : DocumentReference = db.collection("app_board").document(boardData.documentId)
+
+        likesReference.get().addOnCompleteListener{result ->
+            likeCount = 0
+            for (documentSnapshot in result.getResult()) {
+                // 서버에서 좋아요 수를 받아와서 더해준다.
+                likeCount++
+            }
+            val likeString = "$likeCount likes"
+            likes.text = likeString
+        }.addOnFailureListener { error ->
+            Log.e(TAG, "getLikeCountsFromServer: " + error )
+        }
+
+    }
+
+
     private fun pressLikeButton() {
         like.setOnClickListener{
-
+            if(liked) {
+                like.setImageResource(R.drawable.ic_liked)
+                liked = false
+            } else {
+                like.setImageResource(R.drawable.ic_like)
+                liked = true
+            }
         }
     }
 
     private fun pressSaveButton() {
-
+        if(saved) {
+            save.setImageResource(R.drawable.ic_save_black)
+            saved = false
+        } else {
+            save.setImageResource(R.drawable.ic_savee_black)
+            saved = true
+        }
     }
 }
