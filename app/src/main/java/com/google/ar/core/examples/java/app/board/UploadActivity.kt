6e91af73dac62f86cd050ac5f283efd79479135a
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-11-23 20:55:18 +0900

    [ADD]업로드 구현 거의완료

--- app/src/main/java/com/google/ar/core/examples/java/app/board/UploadActivity.kt
+++ app/src/main/java/com/google/ar/core/examples/java/app/board/UploadActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.ar.core.examples.java.app.board.upload.UploadImageViewActivity
 import com.google.ar.core.examples.java.geospatial.R
-import kotlinx.android.synthetic.main.activity_board_click.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class UploadActivity : AppCompatActivity() {
+    private var auth = Firebase.auth
+    private val currentUser = auth.currentUser
+    val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
@@ -21,12 +31,33 @@ class UploadActivity : AppCompatActivity() {
 
         val uploaddata = intent.getSerializableExtra("uploadData") as BoardData?
         println("uploaddata = ${uploaddata}")
+        val imgURL = uploaddata!!.imgURL
 
-        if(uploaddata!=null) {
+        //사진을 고르면 고른 사진을 띄워준다
+        if (uploaddata != null) {
             Glide.with(this).load(uploaddata!!.imgURL).error(R.drawable.ic_baseline_error_outline_24).into(image_added)
         }
+
+
         uploadButton.setOnClickListener {
             println("!! 업로드 버튼 클릭 !!")
+            val now = LocalDateTime.now()
+            val documentID = now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss", Locale.ENGLISH))
+            val data = BoardData(imgURL = imgURL,
+                    description = "업로드 연습용 description1",
+                    likes = 5,
+                    publisher = auth.currentUser!!.uid,
+                    userId = auth.currentUser!!.uid,
+                    documentID)
+
+            db.collection("app_board").document(documentID).set(data!!)
+                    .addOnSuccessListener { documentReference ->
+                        Toast.makeText(this, "게시글 작성완료", Toast.LENGTH_LONG).show()
+                        finish()
+                    }
+                    .addOnFailureListener { e ->
+                        Toast.makeText(this, "게시글 작성 실패", Toast.LENGTH_LONG).show()
+                    }
         }
 
         image_added.setOnClickListener {
@@ -39,7 +70,6 @@ class UploadActivity : AppCompatActivity() {
             println("!! close 클릭 !!")
             finish()
         }
-
 
 
     }
