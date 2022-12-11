e4e6a69a2415df5d920d21511dec9bd45a5b6d65
twoone17 <84762786+twoone17@users.noreply.github.com>
2022-12-11 19:19:22 +0900

    [REACTOR]IMG 밀리는 오류 수정, 튕기는 오류 수정

--- app/src/main/java/com/google/ar/core/examples/java/app/Fragment3.kt
+++ app/src/main/java/com/google/ar/core/examples/java/app/Fragment3.kt
@@ -158,6 +158,9 @@ class Fragment3 : Fragment(), OnMapReadyCallback {
                         if (document != null) {
                             Log.d(TAG, "DocumentSnapshot data: ${document.data}")
                             imgURL= document.data?.get("imgURL")?.toString()
+                            val circle_img : CircleImageView = customDialog.findViewById(R.id.circle_img)
+                            Glide.with(requireContext()).load(imgURL.toString()).error(R.drawable.ic_baseline_error_outline_24).centerCrop().into(circle_img)
+
 //                            likes = document.data?.get("likes")?.toString(),
                         } else {
                             Log.d(TAG, "No such document")
@@ -169,8 +172,8 @@ class Fragment3 : Fragment(), OnMapReadyCallback {
                 Log.e(TAG, "onMarkerClick: tag"+ tag.toString() )
 
                 //포토존 사진 불러오기
-                val circle_img : CircleImageView = customDialog.findViewById(R.id.circle_img)
-                Glide.with(requireContext()).load(imgURL.toString()).error(R.drawable.ic_baseline_error_outline_24).centerCrop().into(circle_img)
+//                val circle_img : CircleImageView = customDialog.findViewById(R.id.circle_img)
+//                Glide.with(requireContext()).load(imgURL.toString()).error(R.drawable.ic_baseline_error_outline_24).centerCrop().into(circle_img)
 
                 //포토존 정보 불러오기
                 val photozoneDetail : TextView = customDialog.findViewById(R.id.photozoneDetail)
@@ -188,6 +191,7 @@ class Fragment3 : Fragment(), OnMapReadyCallback {
                 }
 
                 profileAdapter = ProfileAdapter(requireContext())
+                profileAdapter.notifyDataSetChanged()
                 val recyclerView: RecyclerView = customDialog.recycler_mypost
 
                 profileAdapter.setOnItemClickListener(object : ProfileAdapter.OnItemClickListener{
@@ -198,10 +202,13 @@ class Fragment3 : Fragment(), OnMapReadyCallback {
                         }.run { startActivity(this) }
                     }
                 })
-
+                profileAdapter.notifyDataSetChanged()
                 recyclerView.adapter = profileAdapter
                 val gm = GridLayoutManager(requireContext(), 2)
                 recyclerView.layoutManager = gm
+                profileAdapter.datas.clear()
+                profileAdapter.notifyDataSetChanged()
+                recyclerView.adapter = profileAdapter
 
                 db.collection("photoZone").document(p0.title!!).collection("boardList").get()
                     .addOnSuccessListener { result ->
@@ -211,7 +218,7 @@ class Fragment3 : Fragment(), OnMapReadyCallback {
                                     BoardData(
                                         imgURL = post.get("imgURL").toString(),
                                         description = post?.get("description").toString(),
-                                        likes = post.get("likes") as Long,
+//                                        likes = post.get("likes") as Long,
                                         publisher = post.get("publisher").toString(),
                                         userId = post.get("userId").toString(),
                                         documentId = post.id
